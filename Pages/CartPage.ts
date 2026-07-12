065d2187887251996d8c435bbb1fd87c8fbd4a91
Aishwarya <aishwaryapatil5201@gmail.com>
2026-07-12 22:36:59 +0530

    update

--- Pages/CartPage.ts
+++ Pages/CartPage.ts
@@ -1,16 +1,35 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
+
 export class CartPage extends BasePage {
+
 
     constructor(page: Page) {
         super(page);
     }
 
-    proceedCheckoutButton = this.page.locator('.check_out');
+
+    proceedCheckoutButton =
+        this.page.locator("a.check_out");
+
 
     async proceedCheckout() {
-        await this.proceedCheckoutButton.scrollIntoViewIfNeeded();
+
+
+        await expect(this.page)
+            .toHaveURL(/view_cart/);
+
+
+        await expect(
+            this.proceedCheckoutButton
+        ).toBeVisible({
+            timeout:30000
+        });
+
+
         await this.proceedCheckoutButton.click();
+
     }
+
 }
--- Pages/LoginPage.ts
+++ Pages/LoginPage.ts
@@ -1,35 +1,51 @@
-import { Page } from '@playwright/test';
-import { BasePage } from './BasePage';
+import { Page, expect } from "@playwright/test";
+import { BasePage } from "./BasePage";
+
 
 export class LoginPage extends BasePage {
 
-    constructor(page: Page) {
+
+    constructor(page:Page){
         super(page);
     }
 
-    email = this.page.locator("[data-qa='login-email']");
-    password = this.page.locator("[data-qa='login-password']");
-    loginButton = this.page.locator("[data-qa='login-button']");
-    loggedIn = this.page.locator("a:has-text('Logged in as')");
 
-    async loginUser(username: string, pwd: string) {
+    email =
+        this.page.locator("[data-qa='login-email']");
 
-    await this.fill(this.email, username);
 
-    await this.fill(this.password, pwd);
+    password =
+        this.page.locator("[data-qa='login-password']");
 
-    await this.click(this.loginButton);
 
-    await this.page.waitForLoadState("networkidle");
+    loginButton =
+        this.page.locator("[data-qa='login-button']");
 
-}
 
-    
+    loggedIn =
+        this.page.locator("a:has-text('Logged in as')");
 
-    
 
-    
 
-    
+    async loginUser(username:string,password:string){
+
+
+        await this.email.fill(username);
+
+
+        await this.password.fill(password);
+
+
+        await this.loginButton.click();
+
+
+        await expect(
+            this.loggedIn
+        ).toBeVisible({
+            timeout:30000
+        });
+
+
+    }
 
 }
